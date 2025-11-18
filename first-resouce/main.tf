9b32365f46bbef55751e804571ff921fcf63ff58
Mslliksrjun <mallikarjunckm@example.com>
2025-11-18 16:18:49 +0530

    added tag name

---
+++ first-resouce/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAU6PTT67RSCKCCCUF"
+  secret_key = "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"
+  
+}
+resource "aws_vpc" "myvpc" {
+    cidr_block = "20.0.0.0/16"
+    tags = {
+      "name" = "terrafrom-ec2"
+    }
+  
+}
